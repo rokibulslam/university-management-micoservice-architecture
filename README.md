b6b831a3138a836a3af4be17978f25015717819b
rokibulslam <rokibulslam6@gmail.com>
2023-06-11 11:08:07 +0600

    server setup

--- README.md
+++
@@ -1,5 +0,0 @@
-# University-Management-System ---> Auth-Service
-# Microservice Architecture
-### Development Mode
-# See Branch for details
-# Branch Name Taken from Jira Sprint
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
 const app: Application = express();
+const port = 3000;
 
 app.use(cors());
 
-// parser
+//parser
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
-// Testing
+//Testing
 app.get("/", (req: Request, res: Response) => {
-    res.send("Server Working Successfullly")
-})
+  res.send("Server running Successfully");
+});
 
 export default app;
-
